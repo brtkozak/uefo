02013381a6d631d64fcf425bd39007164e2dc058
mekiert <mateusz.ekiert@iteratec.com>
2021-04-18 12:30:05 +0200

    microservices init

---
+++ atendees/src/main/kotlin/com/example/atendees/AtendeesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.atendees
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtendeesApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtendeesApplication>(*args)
+}
---
+++ matches/src/main/kotlin/com/example/matches/MatchesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.matches
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchesApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatchesApplication>(*args)
+}
